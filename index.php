dc7f33d0951d1ea21369ce53f72abc788b4dd1be
Flavio Pinto <flaviopnt@hotmail.it>
2020-05-28 16:40:30 +0200

    Starting project

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dist/assets/styles/styles.css">
+    <title>PHP MYSQL CRUD</title>
+</head>
+<body>
+    
+    <!-- JAVASCRIPT -->
+    <script src="dist/assets/scripts/main.js"></script>
+</body>
+</html>
---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+
+$(document).ready(function () {
+    console.log('JQUERY IT\'S IN THE GAME!');
+    
+}); //<--- end doc ready
